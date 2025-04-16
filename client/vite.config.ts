2ef416c5f9db6b5fa75c0aebc1bfea6136525957
ShreyaReddy9 <gudurushreya.16@gmail.com>
2025-04-15 20:26:55 -0700

    Configure GitHub Pages deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/rsvp_service/',  // <-- MUST match your GitHub repo name
   plugins: [react()],
-  server: {
-    port: 3000
-  }
 });
